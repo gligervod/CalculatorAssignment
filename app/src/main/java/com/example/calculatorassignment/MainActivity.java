5c4e129683a546d7e8df08b1c3226effe83ddef5
Gavin <90773249+gligervod@users.noreply.github.com>
2022-08-23 09:27:00 -0500

    Add + sub finished
    math works, still crashes when wrong data type is entered, either restrict inputs or add try/catch

--- app/src/main/java/com/example/calculatorassignment/MainActivity.java
+++ app/src/main/java/com/example/calculatorassignment/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.calculatorassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void findSum(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView resultTV = findViewById(R.id.resultTV);
+        int num1 = Integer.parseInt(number1ET.getText().toString());
+        int num2 = Integer.parseInt(number2ET.getText().toString());
+        int sum = num1 + num2;
+        resultTV.setText("" + sum);
+    }
+
+    public void findDif(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView resultTV = findViewById(R.id.resultTV);
+        int num1 = Integer.parseInt(number1ET.getText().toString());
+        int num2 = Integer.parseInt(number2ET.getText().toString());
+        int dif = num1 - num2;
+        resultTV.setText("" + dif);
+    }
+
 }
