ddd879e65cd1d55c01526af4619bf3dedaeb8d6c
Gavin <90773249+gligervod@users.noreply.github.com>
2022-08-24 08:20:33 -0500

    Math
    Math fully functioning, added signs to layout

--- app/src/main/java/com/example/calculatorassignment/MainActivity.java
+++ app/src/main/java/com/example/calculatorassignment/MainActivity.java
@@ -9,30 +9,51 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText number1ET;
+    EditText number2ET;
+    TextView resultTV;
+    TextView operationTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        number1ET = findViewById(R.id.num1ET);
+        number2ET = findViewById(R.id.num2ET);
+        resultTV = findViewById(R.id.resultTV);
+        operationTV = findViewById(R.id.operationTV);
     }
 
     public void findSum(View view) {
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
-        TextView resultTV = findViewById(R.id.resultTV);
         int num1 = Integer.parseInt(number1ET.getText().toString());
         int num2 = Integer.parseInt(number2ET.getText().toString());
         int sum = num1 + num2;
-        resultTV.setText("" + sum);
+        resultTV.setText("=\t\t" + sum);
+        operationTV.setText("+");
     }
 
     public void findDif(View view) {
-        EditText number1ET = findViewById(R.id.num1ET);
-        EditText number2ET = findViewById(R.id.num2ET);
-        TextView resultTV = findViewById(R.id.resultTV);
         int num1 = Integer.parseInt(number1ET.getText().toString());
         int num2 = Integer.parseInt(number2ET.getText().toString());
         int dif = num1 - num2;
-        resultTV.setText("" + dif);
+        resultTV.setText("=\t\t" + dif);
+        operationTV.setText("-");
     }
+
+    public void findmult(View view){
+        int num1 = Integer.parseInt(number1ET.getText().toString());
+        int num2 = Integer.parseInt(number2ET.getText().toString());
+        int mult = num1 * num2;
+        resultTV.setText("=\t\t" + mult);
+        operationTV.setText("×");
+    }
+
+    public void findDiv(View view){
+        int num1 = Integer.parseInt(number1ET.getText().toString());
+        int num2 = Integer.parseInt(number2ET.getText().toString());
+        double div = (double)num1 / num2;
+        resultTV.setText("=\t\t" + div);
+        operationTV.setText("÷");
+    }
+
 
 }
