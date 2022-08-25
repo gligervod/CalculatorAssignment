6e080e56dbf7772a2d062c448e0beeb2a47ef6eb
Gavin <90773249+gligervod@users.noreply.github.com>
2022-08-25 07:59:48 -0500

    Extra touches
    try/catch blocks added &working, remainder divide option added

--- app/src/main/java/com/example/calculatorassignment/MainActivity.java
+++ app/src/main/java/com/example/calculatorassignment/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText number2ET;
     TextView resultTV;
     TextView operationTV;
+    Switch intDiv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,38 +23,55 @@ public class MainActivity extends AppCompatActivity {
         number2ET = findViewById(R.id.num2ET);
         resultTV = findViewById(R.id.resultTV);
         operationTV = findViewById(R.id.operationTV);
+        intDiv = findViewById(R.id.IntDivSwitch);
     }
 
     public void findSum(View view) {
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        int sum = num1 + num2;
-        resultTV.setText("=\t\t" + sum);
-        operationTV.setText("+");
+        try {
+            int num1 = Integer.parseInt(number1ET.getText().toString());
+            int num2 = Integer.parseInt(number2ET.getText().toString());
+            int sum = num1 + num2;
+            resultTV.setText("=\t\t" + sum);
+            operationTV.setText("+");
+        } catch (Exception e){}
     }
 
     public void findDif(View view) {
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        int dif = num1 - num2;
-        resultTV.setText("=\t\t" + dif);
-        operationTV.setText("-");
+        try {
+            int num1 = Integer.parseInt(number1ET.getText().toString());
+            int num2 = Integer.parseInt(number2ET.getText().toString());
+            int dif = num1 - num2;
+            resultTV.setText("=\t\t" + dif);
+            operationTV.setText("-");
+        } catch (Exception e){}
     }
 
     public void findmult(View view){
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        int mult = num1 * num2;
-        resultTV.setText("=\t\t" + mult);
-        operationTV.setText("×");
+        try {
+            int num1 = Integer.parseInt(number1ET.getText().toString());
+            int num2 = Integer.parseInt(number2ET.getText().toString());
+            int mult = num1 * num2;
+            resultTV.setText("=\t\t" + mult);
+            operationTV.setText("×");
+        } catch (Exception e){}
     }
 
     public void findDiv(View view){
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        double div = (double)num1 / num2;
-        resultTV.setText("=\t\t" + div);
-        operationTV.setText("÷");
+        try {
+            if(intDiv.isChecked()){
+                int num1 = Integer.parseInt(number1ET.getText().toString());
+                int num2 = Integer.parseInt(number2ET.getText().toString());
+                int div = num1 / num2;
+                resultTV.setText("=\t\t" + div + " r" + num1 % num2);
+                operationTV.setText("÷");
+            } else {
+                int num1 = Integer.parseInt(number1ET.getText().toString());
+                int num2 = Integer.parseInt(number2ET.getText().toString());
+                double div = (double) num1 / num2;
+                resultTV.setText("=\t\t" + div);
+                operationTV.setText("÷");
+            }
+        } catch (Exception e){}
     }
 
 
